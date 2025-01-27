15a1cd21ff3a00ac0dfaa3b9ae09befef3cf3337
Zitcheron <adhamastito55@gmail.com>
2025-01-27 23:44:48 +0700

    pertamax

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [
+    "index.html",
+    "./src/**/*.{html,js}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      boxShadow: {
+        custom: "14px 14px 0 rgba(0, 0, 0, 0.2)", // Posisi khusus
+        "custom-inset": "inset 5px 5px 0 rgba(0, 0, 0, 0.3)", // Shadow inset
+      },
+      backgroundPosition: {
+        "custome-center": "50% 50%",
+      },
+      colors: {
+        mainColor: "#FF7A00",
+        secondaryColor: "#3F4F6F",
+        ascentColor: "#F09C4E",
+      },
+      fontFamily: {
+        poppins: ["poppins"],
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [require("flowbite/plugin"), "prettier-plugin-tailwindcss"],
+};
